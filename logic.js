9f2366646eb7bd68ab454354e4b8d088f3c419b4
unknown <archivecoderzero@gmail.com>
2019-05-09 16:46:43 -0700

    bug fixes

--- logic.js
+++ logic.js
@@ -2,11 +2,14 @@ function submit ( ) {
     username = $("#username").val().trim();
     projectname = $("#projectname").val().trim();
     var resultDiv = $("<li>");
+    var linkable = $("<a>");
     var the = "https://" + username + ".github.io/" + projectname + "/" ;
-    resultDiv.attr("href", the);
-    resultDiv.attr("target","_blank")
-    resultDiv.append(the)
+    linkable.attr("href", the);
+    linkable.attr("target","_blank")
+    linkable.append(the)
     $("#results").append(resultDiv);
+    resultDiv.append(linkable);
+
 }
 
 $(document).on("click", "#submit", submit);
